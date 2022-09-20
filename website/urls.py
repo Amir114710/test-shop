4215b1388a855f0a69cb5a216274bf688499e3b2
Rasta724.com <97888az@gmail.com>
2022-09-21 00:56:16 +0430

    Start filter2 branch

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_view, name='home' ),
+    path('', views.HomeView.as_view(), name='home' ),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+# # Create your views here.
+# def home_view(request):
+#     return render(request,'home.html')
 
-# Create your views here.
-def home_view(request):
-    return render(request,'home.html')
+class HomeView(TemplateView):
+    template_name = 'home.html'
