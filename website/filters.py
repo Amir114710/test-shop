7dee08adf5d9d61fd8bec0235a24c38480867aea
Rasta724.com <97888az@gmail.com>
2022-09-26 15:53:47 +0330

    end of the project 3

--- website/filters.py
+++ website/filters.py
@@ -2,7 +2,6 @@ import django_filters
 from website.models import Product
 
 class ProductsFilter(django_filters.FilterSet):
-    
     class Meta:
         model = Product
         fields = {'categories':['exact'] , 'size':['exact'] ,  'color':['exact'] , 'price':['exact']}
